275b0ba19a06f55a9d98e83890b6473875a2efd1
Tu Pham <minhtu.bk14@gmail.com>
2025-11-23 17:04:55 +0700

    add

--- xe_lidar/launch/launch_robot.launch.py
+++ xe_lidar/launch/launch_robot.launch.py
@@ -59,22 +59,6 @@ def generate_launch_description():
     print("   5. Joint State Broadcaster - sau khi controller manager start")
     print("=" * 60)
 
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
     # Launch them all!
     return LaunchDescription([rsp, # joystick,
         twist_mux, delayed_controller_manager, delayed_ackermann_controller_spawner, delayed_joint_broad_spawner])
